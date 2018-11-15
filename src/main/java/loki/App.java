7f5a250c860bbf2864747ef78e028bd6c4bddb6e
ryanwren <rwren@cloudbees.com>
2018-11-15 13:52:44 -0500

    QE-558 Testing SEC-400 changes

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -2,12 +2,10 @@ package loki;
 
 /**
  * Hello world Application!!!
+ * QE-558
  */
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-	System.out.println( "This is the Loki App!!" );
     }
 }
